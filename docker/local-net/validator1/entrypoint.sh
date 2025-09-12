3a5c1bb6545ed956965f3fcf6bd23a0c7f9daf66
luispdm <17044119+luispdm@users.noreply.github.com>
2025-09-12 17:43:46 +0200

    - 2 validators & 1 fullnode - tools testing not needed anymore - fullnode yaml config TBD: - unify validators entrypoint

--- docker/local-net/validator1/entrypoint.sh
+++ docker/local-net/validator1/entrypoint.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-VALIDATOR=$(sui validator display-metadata 2>&1)
-ACCOUNT="0xdbe3e8dd0739620448d202d18158b9622e65ea797f2571d3574c4e4d33e0fb09"
-
-if echo "$VALIDATOR" | grep -q "is not an active or pending Validator"; then
-    SUI_PATH="$HOME/.sui/sui_config"
-    cd $SUI_PATH
-
-    # Generate validator info and key files
-    sui validator make-validator-info $ACCOUNT "" "https://example.com/image.png" "https://example.com" "validator1" 1000
-
-    # Make that validator join the committee
-    sui validator become-candidate $SUI_PATH/validator.info
-
-    # Stake SUI to the validator pool (the command uses the last gas coin owned by the address, make sure it has at least 30M SUI)
-    sui client call --package 0x3 --module sui_system --function request_add_stake --args 0x5 $(sui client gas | grep 0x | awk '{print $2}' | tail -1) $ACCOUNT
-
-    # Join the committee
-    sui validator join-committee
-else
-    echo "$ACCOUNT is already an active or pending validator, no need to join committee again"
-fi
+sed -i 's/metrics-address: ".*"/metrics-address: "0.0.0.0:9185"/' /opt/sui/config/validator1-8080.yaml
+sed -i -E 's#^([[:space:]]*genesis-file-location:[[:space:]]*).*$#\1/opt/sui/config/genesis.blob#' /opt/sui/config/validator1-8080.yaml
 
 exec "$@"
