ba0dfceadca53a9d22284f7a61af8f72495e34ae
luispdm <17044119+luispdm@users.noreply.github.com>
2025-09-23 17:47:57 +0200

    - configurable epoch duration - three validators

--- docker/local-net/genesis/entrypoint.sh
+++ docker/local-net/genesis/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /root/.sui/sui_config/genesis.blob ]; then
     echo "Creating new genesis..."
-    sui genesis --from-config /root/genesis.yaml -f --with-faucet
+    sui genesis --epoch-duration-ms $EPOCH --from-config /root/genesis.yaml -f --with-faucet
 else
     echo "Genesis files already exist, skipping initialization"
 fi
